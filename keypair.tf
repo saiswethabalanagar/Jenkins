fce87d206b21e69d2aa8c43d44d888dbc282517f
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-18 01:47:55 -0500

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "my_key_pair1" {
-  key_name   = "my-key-pair1"  # Change to your desired key pair name
+resource "aws_key_pair" "my_key_pair12" {
+  key_name   = "my-key-pair12"  # Change to your desired key pair name
   public_key = file("./id_rsa.pub")  # Path to your public key file
 }
