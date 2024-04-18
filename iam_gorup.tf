fb3503715a1f1cda5370caab921f755d68887286
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-17 21:04:54 -0500

    Update iam_gorup.tf

--- iam_gorup.tf
+++ iam_gorup.tf
@@ -1,11 +1,8 @@
 resource "aws_iam_group" "new_group" {
   name = "new_group_name"
 }
+
 resource "aws_iam_group_policy_attachment" "attach_group_policy" {
   group      = aws_iam_group.new_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"  # Example policy, replace with your desired policy ARN
-}
-resource "aws_iam_group_membership" "add_user_to_group" {
-  name  = aws_iam_group.new_group.name
-  users = [aws_iam_user.new_user.name]
 }
