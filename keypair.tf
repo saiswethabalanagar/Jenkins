142db8e04767edf57bfa7fe2f4cce82caf1e3156
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-17 23:55:10 -0500

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "my-key-pair"
-  public_key = file("./id_rsa.pub")  # Relative path to the id_rsa.pub file
+  public_key = file("./id_rsa.pub")  # Path to your public key file
+  private_key = file("./privatekey.pem")  # Path to your private key file
 }
-
