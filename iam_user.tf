6c6efd760e0a969b039d5f757feba977e386534b
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-17 20:58:31 -0500

    Update iam_user.tf

--- iam_user.tf
+++ iam_user.tf
@@ -1,3 +1,7 @@
 resource "aws_iam_user" "example" {
   name = "example"
 }
+resource "aws_iam_user_policy_attachment" "example_attachment" {
+  user       = aws_iam_user.example.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # Example policy, replace with your desired policy ARN
+}
