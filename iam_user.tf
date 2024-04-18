f2e25619e5116862439842dd546f251e782930e3
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-17 21:05:51 -0500

    Update iam_user.tf

--- iam_user.tf
+++ iam_user.tf
@@ -1,7 +1,13 @@
 resource "aws_iam_user" "example" {
   name = "example"
 }
+
 resource "aws_iam_user_policy_attachment" "example_attachment" {
   user       = aws_iam_user.example.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # Example policy, replace with your desired policy ARN
+}
+
+resource "aws_iam_group_membership" "add_user_to_group" {
+  name  = aws_iam_group.new_group.name
+  users = [aws_iam_user.example.name]
 }
