f7d76c5ceb98d4df31f98997c2428f9f3e7097b6
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-18 02:24:38 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,18 @@
-
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_block
+  cidr_block        = local.public_subnet_cidr
   availability_zone = "${var.region}a"
 }
 
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_block
+  cidr_block        = local.public_subnet_cidr
   availability_zone = "${var.region}b"
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr_block
+  cidr_block        = local.private_subnet_cidr
   availability_zone = "${var.region}c"
 }
 
@@ -30,19 +29,18 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "route_internet" {
-  for_each = aws_subnet.public1
+  for_each = { for idx, subnet in aws_subnet.public1 : idx => subnet }
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
 resource "aws_eip" "nat" {
-  vpc = true
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public1.id  # Use one of the public subnets for NAT Gateway
+  subnet_id     = aws_subnet.public1.id
 }
 
 resource "aws_route" "route_nat" {
@@ -51,8 +49,21 @@ resource "aws_route" "route_nat" {
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
-resource "aws_route_table_association" "public" {
-  for_each = aws_subnet.public1
-  subnet_id      = each.value.id
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_eip" "unused_eip" {
+  instance = null
+}
+
