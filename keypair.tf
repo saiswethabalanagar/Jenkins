ea58e5e0fb27421be83c978f5509fa2df2a920d3
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-18 01:02:34 -0500

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,6 @@
 resource "aws_key_pair" "my_key_pair" {
-  key_name   = "my-key-pair"
-  public_key = file("./id_rsa.pub")  # Path to your public key file
- } 
+  count = var.create_key_pair ? 1 : 0  # Conditionally create the key pair based on the variable
+
+  key_name   = "my-key-pair"  # Change to your desired key pair name
+  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key file
+}
